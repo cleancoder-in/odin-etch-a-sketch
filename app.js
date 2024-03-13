60db57c95366034e622c2fcfa24c99da6929ba02
Anurag <in.cleancoder@gmail.com>
2024-03-13 16:33:17 +0530

    feat: add the functionality of coloring the cells with random RGB values and add the progressive darkening effect

--- app.js
+++ app.js
@@ -1,43 +1,85 @@
 const gridContainerEl = document.querySelector(".grid-wrapper");
-const createGridBtnEl = document.querySelector("button");
-let flexBasisValue;
+const newGridBtnEl = document.querySelector(".newGridBtn");
+const clearGridBtnEl = document.querySelector(".clearGridBtn");
+const cells = [];
+let initialGridNum = 16;
+let rgbColor = "";
 const MAX_GRID_NUMBER = 100;
+let decreasingAmount = 0;
 
-function getFlexBasisValue(num) {
-  flexBasisValue = Math.floor(100 / num);
-  return flexBasisValue;
+function getRandomColor() {
+  return `rgb(${Math.floor(Math.random() * 256)}, 
+  ${Math.floor(Math.random() * 256)},
+   ${Math.floor(Math.random() * 256)})`;
 }
-function createGrid(num) {
-  let numOfBoxes = num * num;
-  flexBasisValue = getFlexBasisValue(num);
+
+function colorTheCells(cell) {
+  // if cell already colored, then darken it
+  if (cell.classList.contains("marked")) {
+    let rgbArr = cell.style
+      .getPropertyValue("background-color")
+      .match(/\d+/g)
+      .map(Number);
+
+    rgbArr = rgbArr.map((num) => {
+      decreasingAmount = Math.ceil(num / 10);
+      return (num -= decreasingAmount);
+    });
+
+    rgbColor = `rgb(${rgbArr[0]}, ${rgbArr[1]}, ${rgbArr[2]})`;
+    cell.style.removeProperty("background-color");
+    cell.style.removeProperty("border-color");
+    cell.style.cssText += `background-color:${rgbColor}; border-color:${rgbColor}`;
+    // if cell is not colored, then color it
+  } else {
+    rgbColor = getRandomColor();
+    cell.style.cssText += `background-color:${rgbColor}; border-color:${rgbColor}`;
+    cell.classList.add("marked");
+  }
+}
+
+function createGrid(cellsPerSide) {
+  let numOfCells = cellsPerSide * cellsPerSide;
   if (gridContainerEl.children.length > 0) {
     gridContainerEl.innerHTML = "";
   }
-  for (let i = 1; i <= numOfBoxes; i++) {
-    const box = document.createElement("div");
-    box.classList.add("box");
-    box.style.flexBasis = flexBasisValue + "%";
-    box.setAttribute("data-id", i);
-    gridContainerEl.appendChild(box);
+  for (let i = 1; i <= numOfCells; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("box");
+    cell.setAttribute(
+      "style",
+      `width: ${960 / cellsPerSide}px; height: ${960 / cellsPerSide}px`
+    );
+    gridContainerEl.appendChild(cell);
+    cells.push(cell);
   }
+
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseenter", () => {
+      colorTheCells(cell);
+    });
+  });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  createGrid(16);
+  createGrid(initialGridNum);
 });
 
-gridContainerEl.addEventListener("mouseout", (e) => {
-  let boxId = e.target.dataset.id;
-  let boxEl = gridContainerEl.querySelector(`[data-id = "${boxId}"]`);
-  if (boxEl) {
-    boxEl.classList.add("boxColorOnHover");
+newGridBtnEl.addEventListener("click", () => {
+  let customizedGridNum = Number(prompt("Enter a number b/w 1 to 100"));
+  if (customizedGridNum > MAX_GRID_NUMBER) {
+    customizedGridNum = MAX_GRID_NUMBER;
+  } else if (customizedGridNum < 0) {
+    customizedGridNum = 0;
+  } else {
+    createGrid(customizedGridNum);
   }
 });
 
-createGridBtnEl.addEventListener("click", () => {
-  let gridNum = Number(prompt("Create a grid of your choice"));
-  if (gridNum > MAX_GRID_NUMBER) {
-    gridNum = MAX_GRID_NUMBER;
-  }
-  createGrid(gridNum);
+clearGridBtnEl.addEventListener("click", () => {
+  cells.forEach((cell) => {
+    cell.style.removeProperty("background-color");
+    cell.style.removeProperty("border-color");
+    cell.classList.remove("marked");
+  });
 });
